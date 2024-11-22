0787452266c2b5c74be5d06d8e5bbea900baa8e5
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-11-22 19:05:34 +0100

    Aggiunti Controller di Restaurant - Product - Order

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,112 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Recuperiamo tutti i prodotti senza usare 'get()'
+        $products = Product::all();
+        return view('products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+       
+        $request->validate([
+            'name' => 'required|max:255',
+            'ingredients' => 'nullable|max:1000',
+            'price' => 'required|decimal:0,2',
+            'image' => 'nullable|image|max:1024',
+        ]);
+
+        
+        $product = Product::create([
+            'name' => $request->name,
+            'ingredients' => $request->ingredients,
+            'price' => $request->price,
+            'image' => $image,
+        ]);
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+       
+        $product = Product::findOrFail($id);
+        return view('products.show', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        
+        $product = Product::findOrFail($id);
+        return view('products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        
+        $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required|max:1000',
+            'price' => 'required|decimal:0,2',
+            'image' => 'nullable|image|max:1024',
+        ]);
+
+       
+        $product = Product::findOrFail($id);
+
+
+       
+        $product->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price,
+            'image' => $image,
+        ]);
+
+        return redirect()->route('products.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        
+        $product = Product::findOrFail($id);
+
+        
+        $product->delete();
+
+        return redirect()->route('products.index');
+    }
 }
--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -3,8 +3,109 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+//importo il model di Restaurant
+use App\Models\Restaurant;
+
 
 class RestaurantController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $restaurants = Restaurant::all();
+        return view('restaurants.index', compact('restaurants'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('restaurants.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //validazione dati 
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'address' => 'required|string|max:100',
+            'description' => 'nullable|text',
+            'piva' => 'required|string|max:11',
+            'image' => 'nullable|string|1024',
+            'categories' => 'required|array' //controlla se effettivasmente dia un array
+        ]);
+
+        $restaurant = Restaurant::create([
+            'name' => $request->name,
+            'address' => $request->address,
+            'description' => $request->description,
+            'piva' => $request->piva,
+            'image' => $request->image,
+        ]);
+        $restaurant->categories()->sync($request->categories);
+
+        return redirect()->route('restaurants.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $restaurant = Restaurant::findOrFail($id);
+       return view('restaurants.show',compact('restaurant'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $restaurant = Restaurant::findOrFail($id);
+        return view('restaurants.edit', compact('restaurant'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //validazione dati 
+        $request->validate([
+            'name' => 'required|string|max:100',
+            'address' => 'required|string|max:100',
+            'description' => 'nullable|text',
+            'piva' => 'required|string|max:11',
+            'image' => 'nullable|string|1024',
+            'categories' => 'required|array'
+        ]);
+        $restaurant = Restaurant::findOrFail($id);
+
+        $restaurant->update([
+            'name'=> $request->name,
+            'address' => $request->address,
+            'description' => $request->description,
+            'piva'=> $request->piva,
+            'image'=>$request->image
+        ]);
+        $restaurant->categories()->sync($request->categories);
+
+        return redirect()->route('restaurants.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $restaurant = Restaurant::findOrFail($id);
+        $restaurant->delete();
+        return redirect()->route('restaurants.index');
+    }
 }
