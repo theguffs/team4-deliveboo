192b8b85357b264bc057ce09ae07189c8945167b
Riccardo-Mauri <riccardo90mauri@hotmail.it>
2024-12-06 13:12:10 +0100

    Aggiornata grafica

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,17 +10,17 @@
 
 <div class="card">
     <div class="card-header">
-      Registrati
+      <span class="fs-3">Registrati</span>
     </div>
     <div class="card-body">
         <form method="POST" action="{{ route('register') }}" enctype="multipart/form-data">
             @csrf
     
-            <div class="d-flex">
-                <div>
+            <div class="d-flex flex-row justify-content-center">
+                <div class="">
                     <!-- Name -->
                     <div>
-                        <label for="user_name">Nome</label>
+                        <label for="user_name" class="w-100">Nome:</label>
                         <input id="user_name" type="text" name="user_name" value="{{ old('user_name') }}" required autofocus maxlength="255">
                         @error('name')
                             <div>{{ $message }}</div>
@@ -29,7 +29,7 @@
             
                     <!-- Email Address -->
                     <div class="mt-4">
-                        <label for="email">Email</label>
+                        <label for="email" class="w-100">Email:</label>
                         <input id="email" type="email" name="email" value="{{ old('email') }}" required maxlength="255">
                         @error('email')
                             <div>{{ $message }}</div>
@@ -38,7 +38,7 @@
             
                     <!-- Password -->
                     <div class="mt-4">
-                        <label for="password">Password</label>
+                        <label for="password" class="w-100">Password:</label>
                         <input id="password" type="password" name="password" required minlength="8">
                         @error('password')
                             <div>{{ $message }}</div>
@@ -47,13 +47,13 @@
             
                     <!-- Confirm Password -->
                     <div class="mt-4">
-                        <label for="password_confirmation">Conferma Password</label>
+                        <label for="password_confirmation" class="w-100">Conferma Password:</label>
                         <input id="password_confirmation" type="password" name="password_confirmation" required>
                     </div>
             
                     <!-- Restaurant Name -->
                     <div class="mt-4">
-                        <label for="name">Nome Ristorante</label>
+                        <label for="name" class="w-100">Nome Ristorante:</label>
                         <input id="name" type="text" name="name" value="{{ old('name') }}" required maxlength="100">
                         @error('restaurant_name')
                             <div>{{ $message }}</div>
@@ -62,7 +62,7 @@
             
                     <!-- Address -->
                     <div class="mt-4">
-                        <label for="address">Indirizzo</label>
+                        <label for="address" class="w-100">Indirizzo:</label>
                         <input id="address" type="text" name="address" value="{{ old('address') }}" required maxlength="100">
                         @error('address')
                             <div>{{ $message }}</div>
@@ -71,7 +71,7 @@
             
                     <!-- Description -->
                     <div class="mt-4">
-                        <label for="description">Descrizione</label>
+                        <label for="description" class="w-100">Descrizione:</label>
                         <textarea id="description" name="description" maxlength="255">{{ old('description') }}</textarea>
                         @error('description')
                             <div>{{ $message }}</div>
@@ -80,7 +80,7 @@
             
                     <!-- PIVA -->
                     <div class="mt-4">
-                        <label for="piva">Partita IVA</label>
+                        <label for="piva" class="w-100">Partita IVA:</label>
                         <input id="piva" type="text" name="piva" value="{{ old('piva') }}" required size="11">
                         @error('piva')
                             <div>{{ $message }}</div>
@@ -105,10 +105,12 @@
                         <label for="categories">Categorie del Ristorante</label>
                         <div id="categories">
                             @foreach($categories as $category)
-                                <div>
-                                    <input type="checkbox" id="category{{ $category->id }}" name="categories[]" value="{{ $category->id }}">
-                                    <label for="category{{ $category->id }}">{{ $category->name }}</label>
-                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="flexCheckDefault category{{ $category->id }}" name="categories[]" value="{{ $category->id }}">
+                                    <label class="form-check-label" for="flexCheckDefault category{{ $category->id }}">
+                                        {{ $category->name }}
+                                    </label>
+                                  </div>
                             @endforeach
                         </div>
                         @error('categories')
@@ -117,16 +119,17 @@
                     </div>
                 </div>
             </div>
-    
-            <div class="mt-4">
-                <a href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-    
-                <button type="submit" class="ms-4">
-                    Register
-                </button>
+            <div class="mt-4 px-5 justify-content-center">
+                <div class="px-5 mx-5">
+                    <button type="submit" class="ms-4 border border-0 px-3 py-2 bg-success text-light rounded-pill">
+                        Register
+                    </button>
+                    <a href="{{ route('login') }}" class="link-underline-light fs-6">
+                       <span class="fs-6 text-black">{{ __('Sei già registrato?') }}</span> 
+                    </a>
+                </div>
             </div>
+    
         </form>
     </div>
 </div>
